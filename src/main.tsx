9924ec419c2052a4795c21a7ba250359a62a19a1
Iska Mabo <maboiskanderr@gmail.com>
2025-09-15 02:08:26 +0600

    Refactor: theme config provider

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,30 @@ import 'antd/dist/reset.css'
 import { App } from '@/pages/root/App'
 import '@/shared/lib/notify'
 import '@/shared/styles/index.scss'
+import { ConfigProvider, theme } from 'antd'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider
+      theme={{
+        algorithm: theme.darkAlgorithm,
+        token: {
+          colorPrimary: "var(--brand)",
+          colorTextLightSolid: "var(--text)",
+        },
+        components: {
+          Button: {
+            defaultHoverColor: "var(--text)",
+            defaultHoverBg: "var(--brand)",
+            defaultHoverBorderColor: "var(--brand)",
+            defaultActiveColor: "var(--text)",
+            defaultActiveBg: "var(--brand)",
+            defaultActiveBorderColor: "var(--brand)",
+          },
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
   </React.StrictMode>,
 )
